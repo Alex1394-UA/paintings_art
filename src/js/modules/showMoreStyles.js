9126eea8cc60f29651c2cb803eaf5b702580e434
Alexander <homeholder@bigmir.net>
2022-01-28 20:34:35 +0200

    load more pics added from json-server

--- src/js/modules/showMoreStyles.js
+++ src/js/modules/showMoreStyles.js
@@ -1,8 +1,10 @@
-const showMoreStyles = (trigger, styles) => {
-    const cards = document.querySelectorAll(styles),
-          btn = document.querySelector(trigger);
+import { getResource } from "../services/requests";
 
-    cards.forEach(card => {
+const showMoreStyles = (trigger, wrapper) => {
+    const btn = document.querySelector(trigger);
+        //   cards = document.querySelectorAll(styles); - для предыдущей версии
+
+   /*  cards.forEach(card => {
         card.classList.add('animated', 'fadeInUp');
     });
 
@@ -13,7 +15,42 @@ const showMoreStyles = (trigger, styles) => {
         });
         // btn.style.display = 'none';
         btn.remove();
+    }); */
+
+    btn.addEventListener('click', function() {
+        // getResource('http://localhost:3000/styles') - вариант с сервером
+        getResource('assets/db.json')
+            .then(res => createCards(res.styles)) //.styles для варинта без серв
+            .catch(error => console.log(error));
+
+        this.remove();
     });
+
+    function createCards(response) {
+        response.forEach(({src, title, link}) => {
+            let card = document.createElement('div');
+
+            card.classList.add('animated', 'fadeInUp','col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
+
+            card.innerHTML = `
+                <div class="styles-block">
+                    <img src=${src} alt="style">
+                    <h4>${title}</h4>
+                    <a href=${link}>Подробнее</a>
+                </div>
+            `;
+
+            document.querySelector(wrapper).appendChild(card);
+        });
+    }
+
+        /* <div class="hidden-lg hidden-md hidden-sm hidden-xs styles-2">
+            <div class=styles-block>
+                <img src=assets/img/styles-6.jpg alt>
+                <h4>Поп-арт</h4>
+                <a href="#">Подробнее</a>
+            </div>
+        </div> */
 };
 
 export default showMoreStyles;
